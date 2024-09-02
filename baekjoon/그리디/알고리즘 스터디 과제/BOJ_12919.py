b60d8b0eee82daddf12b46359b622d9489c84ae4
jinyoung_jung <ya_1001@naver.com>
2024-09-02 20:35:43 +0900

    240902 BOJ_12919 실패

--- baekjoon/그리디/알고리즘 스터디 과제/BOJ_12919.py
+++ baekjoon/그리디/알고리즘 스터디 과제/BOJ_12919.py
@@ -5,51 +5,36 @@ input = sys.stdin.readline
 
 # reverse 를 위한 리스트화
 S = input().strip()
-T = list(input().strip())
+T = input().strip()
 # S -> T 로 바꿈
 
-## 1. A 추가
-## 2. B 추가 후 모든 문자열 뒤집기
-
-
-def pop(arr):
-    arr.pop()
-    return T
-
-def reverse(arr):
-    arr.reverse()
-    arr.pop()
-    return arr
-
-
-def check(S,T):
+def dfs(str1, str2):
     global result
-
-    if result or len(S) > len(T):
-        return
-
-    if len(S) == len(T):
-        temp = "".join(T)
-        if temp == S :
+    if len(str1) == len(str2) :
+        if str1 == str2 :
             result = True
         return
 
-    if T[-1] == "B":
-        check(S,reverse(T))
-    elif T[-1] == "A":
-        print(T)
-        if T[0] == "B":
-            print("check B")
-            check(S,reverse(T))
+    if str2[-1] == "B":
+        str3= str2[::-1]
+        dfs(str1,str3[:-1])
 
-        check(S,pop(T))
+    elif str2[-1] == "A":
+        # 그냥 A 제거
+        str3 = str2[:-1]
+        dfs(str1, str3)
+        if str2[0] == "B":
+            # 뒤집고 제거
+            str3 = str2[::-1]
+            str3 = str3[:-1]
+            dfs(str1,str3)
+
+
 
 result = False
-check(S,T)
+dfs(S,T)
 
-if result :
+if result:
     print(1)
 else:
     print(0)
-
-
