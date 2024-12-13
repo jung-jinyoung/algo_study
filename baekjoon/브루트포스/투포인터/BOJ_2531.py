431e88a1f5008efa8b519e9dca3921e7b06641b8
Jung Jinyoung <ya_1001@naver.com>
2024-12-13 11:55:05 +0900

    BOJ 2531 solved

--- baekjoon/브루트포스/투포인터/BOJ_2531.py
+++ baekjoon/브루트포스/투포인터/BOJ_2531.py
@@ -6,7 +6,6 @@ input = sys.stdin.readline
 n, d, k, c = map(int, input().strip().split())
 # 접시의 수 n, 초밥 가짓수 d 연속해서 먹는 접시의 수 k, 쿠폰 번호 c
 
-# 컨베이어 벨트 덱 
 sushies = [int(input()) for _ in range(n)]
 max_result = 0
 for i in range(n):
