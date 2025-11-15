a1800d4df5136dd029127a7fbf9ac30467838707
jinyoung jung <ya_1001@naver.com>
2025-11-16 00:31:22 +0900

    feat: boj 11659 구간 합 구하기 4 해결

---
+++ baekjoon/구현/누적합/BOJ 11659.py
@@ -0,0 +1,16 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+n, m = map(int, input().split(" "))
+nums = list(map(int, input().split(" ")))
+
+
+prefix = [0] * (n+1)
+for i in range(1, n+1):
+    prefix[i] = prefix[i-1] + nums[i-1]
+
+for _ in range(m) :
+    i, j = map(int, input().split(" "))
+    print(prefix[j] - prefix[i-1])
